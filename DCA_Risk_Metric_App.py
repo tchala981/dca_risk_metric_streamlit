6d07e2f77b8ea3fd027a4aefcc7ba002564c4b7b
Atanu Gayen <22393834+tchala981@users.noreply.github.com>
2023-12-16 14:05:05 +0530

    added first version of the app

---
+++ DCA_Risk_Metric_App.py
@@ -0,0 +1,198 @@
+# main page.py
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from scipy.stats import hmean, percentileofscore
+import matplotlib.pyplot as plt
+import streamlit as st
+import plotly.graph_objects as go
+import plotly.express as px
+import io
+
+# st.set_page_config(layout="wide")
+
+def main():
+
+    # Your existing functions (prepare_data_for_analysis, plot_historical_percentile_risk, plot_simplified_risk) go here
+    def prepare_data_for_analysis(scrip, interval, window):
+        def calculate_harmonic_mean(data):
+            return data['Close'].rolling(window=window).apply(hmean, raw=True)
+
+        def calculate_ln_close_hm(data):
+            return np.log(data['Close'] / data['Harmonic_Mean'])
+
+        def calculate_historical_percentiles(data, column):
+            historical_percentiles = []
+            for i in range(len(data)):
+                current_slice = data[column].iloc[:i+1].dropna()
+                current_value = data[column].iloc[i]
+                if not np.isnan(current_value):
+                    percentile_rank = percentileofscore(current_slice, current_value, kind='weak')
+                    historical_percentiles.append(percentile_rank)
+                else:
+                    historical_percentiles.append(np.nan)
+            return pd.Series(historical_percentiles, index=data.index)
+
+        # Download data with auto-adjust for dividends and splits
+        # data = yf.download(scrip, interval=interval, auto_adjust=True)[['Close']]
+        data = yf.download(scrip, interval=interval, auto_adjust=True).loc[:,['Close']] 
+
+        # Apply calculations
+        data['Harmonic_Mean'] = calculate_harmonic_mean(data)
+        data['Ln_Close_HM'] = calculate_ln_close_hm(data)
+        data['deviation_percentile'] = calculate_historical_percentiles(data, 'Ln_Close_HM')
+        data['percentile_risk'] = calculate_historical_percentiles(data, 'deviation_percentile')
+        
+        return data
+
+    def plot_historical_percentile_risk_plotly(data, scrip):
+        fig = go.Figure()
+
+        # Add traces for closing prices and harmonic mean
+        fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Closing Price'))
+        fig.add_trace(go.Scatter(x=data.index, y=data['Harmonic_Mean'], mode='lines', name='Harmonic Mean'))
+
+        # Add scatter trace for percentile risk
+        fig.add_trace(go.Scatter(
+            x=data.index, 
+            y=data['Close'], 
+            mode='markers', 
+            name='Closing Price (Scatter)',
+            marker=dict(color=data['percentile_risk'], colorscale='Viridis'),
+            hoverinfo='text',
+            text=[f'Close: {close}<br>Risk Score: {risk:.2f}' for close, risk in zip(data['Close'], data['percentile_risk'])]
+        ))
+
+        fig.update_layout(
+            title=f'{scrip} Closing Price with Historical Percentile Risk Score (0 to 100)',
+            xaxis_title='Date',
+            yaxis_title='Closing Price',
+            legend=dict(x=0.01, y=0.99, orientation='v', xanchor='left', yanchor='top'),
+            margin=dict(l=100, r=0, t=40, b=0)
+        )
+
+        return fig
+
+    def plot_simplified_risk_plotly(data, scrip):
+        # Define discrete color scale with category names
+        category_colors = {
+            'Very Low Risk': 'blue',
+            'Low Risk': 'green',
+            'Neutral': 'yellow',
+            'High Risk': 'orange',
+            'Very High Risk': 'red'
+        }
+
+        # Map percentile risk to category names
+        data['Risk_Category'] = pd.cut(data['percentile_risk'], 
+                                       bins=[0, 20, 40, 60, 80, 100], 
+                                       labels=list(category_colors.keys()),
+                                       right=False)
+
+        fig = go.Figure()
+
+        # Add a line plot for harmonic mean
+        fig.add_trace(go.Scatter(x=data.index, y=data['Harmonic_Mean'], mode='lines', name='Harmonic Mean', line=dict(color='darkred', width=1)))
+
+        # Add a line plot for closing prices
+        fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Closing Price', line=dict(color='grey', width=1)))
+
+        # Add scatter plot for risk categories
+        for category, color in category_colors.items():
+            category_data = data[data['Risk_Category'] == category]
+            fig.add_trace(go.Scatter(x=category_data.index, y=category_data['Close'], mode='markers', name=category,
+                                     marker=dict(color=color)))
+
+        # Update layout
+        fig.update_layout(
+            title=f'{scrip} Closing Price with Simplified Risk Categories',
+            xaxis_title='Date',
+            yaxis_title='Closing Price',
+            legend=dict(x=0.01, y=0.99, orientation='v', xanchor='left', yanchor='top'),
+            margin=dict(l=100, r=0, t=40, b=0)
+        )
+
+        return fig
+
+
+    # Define a list of tuples with (alias, actual_value)
+    # scrip_options = [
+    #     ('Bitcoin USD', 'BTC-USD'),
+    #     ('Gold Futures (USD)', 'GC=F'),
+    #     ('Gold Bees ETF', 'GOLDBEES.NS'),
+    #     ('Nifty 50 Index', '^NSEI'),
+    #     ('Nifty Bank Index', '^NSEBANK'),
+    #     ('NASDAQ 100', '^NDX'),
+    #     ('S&P 500', '^SPX'),
+    #     ('Dow Jones Industrial', '^DJI')
+    # ]
+    scrip_options = [
+        ('Bitcoin-USD', 'BTC-USD'),
+        # ('Ethereum USD', 'ETH-USD'),
+        ('Gold Futures (USD)', 'GC=F'),
+        ('Silver Futures (USD)', 'SI=F'),
+        ('US 10-Year Treasury', '^TNX'),
+        ('S&P 500', '^GSPC'),
+        ('Dow Jones Industrial Average', '^DJI'),
+        ('NASDAQ 100', '^NDX'),
+        ('Nifty 50 Index', '^NSEI'),
+        ('Sensex', '^BSESN'),
+        ('Nifty Bank', '^NSEBANK'),
+        # ('Nifty IT', '^CNXIT'),
+        # ('Nifty Pharma', '^CNXPHARMA'),
+    ]
+
+
+    # Streamlit app layout
+    st.title("DCA/SIP Risk Metric")
+
+    # Convert the tuple list to a dictionary for easy lookup
+    scrip_dict = dict(scrip_options)
+
+    # Sidebar for user inputs
+    
+    # Asset Selection
+    selected_scrip_alias = st.sidebar.selectbox('Select Asset', options=list(scrip_dict.keys()))
+    scrip = scrip_dict[selected_scrip_alias]  # Get the actual value associated with the alias
+
+    # Interval selection
+    interval_options = ['1d', '1wk']  # List of interval options
+    default_interval_index = interval_options.index('1wk')  # Default to '1wk'
+
+    interval = st.sidebar.selectbox('Select Interval', interval_options, index=default_interval_index)
+
+    st.sidebar.write("Select Moving Average Window Size (between 5 and 2000)")
+    # Number input with hidden label
+    window_input = st.sidebar.number_input('Window Input', min_value=5, max_value=2000, value=200, step=1, key='window_input', label_visibility="collapsed")
+    # # Slider with hidden label
+    # window_slider = st.sidebar.slider('Window Slider', 5, 500, 200, step=1, key='window_slider', label_visibility="collapsed")
+    # # Use whichever input was most recently changed
+    # window = window_slider if st.session_state['window_slider'] == st.session_state['window_input'] else window_input
+    window = window_input
+
+
+    # Function to plot within Streamlit
+    def st_plot(fig):
+        buf = io.BytesIO()
+        fig.savefig(buf, format="png")
+        st.image(buf)
+
+    # Fetch and display data based on user input
+    if st.button('Show Analysis'):
+        btc_data = prepare_data_for_analysis(scrip, interval, window)
+
+        fig2 = plot_simplified_risk_plotly(btc_data, selected_scrip_alias)
+        st.plotly_chart(fig2)
+
+        fig1 = plot_historical_percentile_risk_plotly(btc_data, selected_scrip_alias)
+        st.plotly_chart(fig1)
+        
+
+        # Download as CSV
+        csv = btc_data.to_csv(index=False)
+        b = io.BytesIO(csv.encode())
+        st.download_button("Download data as CSV", b, "btc_data.csv", "text/csv")
+
+
+if __name__ == "__main__":
+    main()
---
+++ pages/Instructions.py
@@ -0,0 +1,96 @@
+# instructions.py
+import streamlit as st
+
+def main():
+    st.title('Instructions and FAQ')
+
+    # General introduction or overview
+    st.write('Welcome to the DCA/SIP Risk Metric Dashboard! This page is designed to help you navigate and use the app effectively.')
+
+    # Specific Instructions
+    with st.expander("Getting Started"):
+        st.write("""
+            **Step 1: Select an Asset**
+            - In the sidebar, choose an asset from the dropdown menu.
+            - Available assets include Bitcoin-USD, Gold Futures, Nifty 50 Index, S&P 500 etc.
+
+            **Step 2: Choose Data Interval and Window Size**
+            - Select the interval for data analysis (e.g., weekly).
+            - Choose the window size for the moving average calculation using the input box.
+
+            **Step 3: Analyze Data**
+            - Click on 'Show Analysis' to generate the charts.
+            - The app will display two interactive charts based on the selected parameters.
+        """)
+
+    with st.expander("Understanding DCA and SIP Strategies"):
+        st.write("""
+            **Dollar Cost Averaging (DCA) and Systematic Investment Plans (SIP)**
+            
+            - DCA and SIP are strategies designed to mitigate market volatility risks by regularly investing a fixed amount into an asset.
+            - They are effective for avoiding market timing risks, benefiting from compounding, and maintaining emotional discipline in investment.
+            - These strategies are ideal for passive investors as they require less frequent trading decisions and market monitoring.
+
+            **Applicability of This App**
+            
+            - The app provides historical percentile risk scores and simplified risk categories, aiding in informed decision-making.
+            - It's useful for scaling into and out of positions, offering insights into potentially lower-risk opportunities.
+            - Users can tailor their investment strategies to align with their risk tolerance and goals.
+        """)
+
+    with st.expander("About the App's Risk Metric"):
+        st.write("""
+            **Why Harmonic Moving Average?**
+            
+            - The app uses the Harmonic Moving Average as a baseline, combined with other techniques to provide a simple risk metric for various assets.
+            - The HMA represents the average cost basis for someone investing a fixed dollar amount over a period of time (the window length) and is thus, especially relevant for DCA strategies.
+
+            **Notes, Guidelines, Warnings for Usage**
+
+            - The names of the risk categories, like "Very Low Risk" or "Very High Risk" are simply an indication of the deviation of the price from mean value, NOT a signal to buy or sell.
+            - As you will notice, an asset can stay in a zone like "Very High Risk" for a very long time, so the price simply entering these zones does not mean you have to exit your whole position.
+            - Predicting tops and bottoms are neither the goal nor the intention of this app.
+            - Instead, these categories are meant as guidelines for risk management. Allowing the user to follow along a chosen systematic gradual entry and exit strategy in an informed manner.
+            - The actual entry and exit strategy (and investment instrument) is for the user to choose after conducting their due diligence. This could be something like, aggressive entry, moderate entry, do nothing, gradual scaling out, at appropriate zones.
+            - The risk categories are guides for understanding the asset's price relative to its historical performance.
+            - They should be used as part of a broader strategy considering the user's specific circumstances and risk tolerance.
+            - The app DOES NOT endorse any specific entry or exit strategy.
+            - Users should conduct their due diligence before investing.
+            - The default settings for the interval (weekly data) and window (200 (weeks)) will be useful for the primary intented use-case for this app: long-term investing.
+            - General guideline is that these 2 parameters should be adjusted to your expected average holding period.
+            
+            **Disclaimer**
+            
+            - The app is for informational purposes and does not endorse any specific trading or investing strategy.
+        """)
+
+    # FAQs Section
+    st.markdown("## Frequently Asked Questions (FAQ)")
+    with st.expander("Q: How do I expand the charts?"):
+        st.write("A: Hover over one of the two charts. On the top left corner, you'll see an arrow. Click on it to expand the chart.")
+
+    with st.expander("Q: How do I zoom in/out, move the chart around, rescale, etc.?"):
+        st.write("A: Upon hovering over the chart, on the top left corner you'll see the tool-box for all these controls.")
+
+    with st.expander("Q: How do I zoom in/out over only one axis easily?"):
+        st.write("A: After selecting the zoom tool from the toolbox, instead of dragging a box somewhere in the middle of the chart, go to the edges, near the x or y-axis and try to drag a box; with a bit of moving around the selection will snap into horizontal/vertical mode.")
+
+    with st.expander("Q: Why are the initial few datapoints in the charts gray/de-coloured?"):
+        st.write("""A: To calculate the moving Harmonic Average, you need atleast enough points equal to the Window length (chosen by the User). So you can't have a classification for those initial points.   
+        - Also, since the calculation of the risk metric involves taking rolling percentiles (multiple times), even the earliest 100 or so Coloured data-points shouldn't be taken seriously. Which shouldn't be a problem, as the User is usually only concerned with the latest status.    
+        - Another point is that the User should use their judgement in making sure the Window length should be much smaller than the number of datapoints available.""")
+
+    with st.expander("Q: Can I download the data used in this app?"):
+        st.write("A: Yes. Intended for research and educational purposes, for personal use only. After the user runs an analysis (after clicking on 'Show Analysis') a button opens at the bottom to: 'Download data as CSV'")
+
+    with st.expander("Q: How frequently is the data updated?"):
+        st.write("""A: The source of the data is Yahoo Finance. You can expect daily updates.   
+            - https://github.com/ranaroussi/yfinance
+            """)
+
+    # Contact Information
+    st.markdown("### Contact Information")
+    st.write("For further queries or feedback, please contact us at [Your Email] or [Other Contact Methods].")
+
+if __name__ == "__main__":
+    main()
